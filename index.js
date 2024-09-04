5bf6f4299c9a3a7bd9bad33827bf6f557b5199cf
Priyavrat Kumar <122096583+Priyavrat001@users.noreply.github.com>
2024-09-04 19:24:18 +0530

    Added crypto payment method

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
 
+dotenv.config();
 
 const app = express();
 const port = 4000;
-app.use(express.json());
-app.use(express.static("public"));
+const cryptoMusURi = process.env.CRYPTO_MUS_URI;
+const serverURi = process.env.Server_URI;
 
-app.use("/", (req, res)=>{
+app.use(express.json());
+app.use(express.static("public/index.html"));
+
+app.get("/", (req, res)=>{
     res.sendFile('public/index.html');
+});
+
+app.post("/checkout",async(req, res, next)=>{
+
+ try {
+    const {product} = req.body;
+
+    const payload = {
+        amount:product.amount,
+        currency:"usd",
+        order_id:"454",
+        url_callback:`${serverURi}/payment/success`
+    }
+
+    const {data} = awiat axios.post(`${cryptoMusURi}/payment`, payload);
+ } catch (error) {
+    res.status(400).json({success:false, message:error.message})
+ }
+
+});
+
+app.post("/payment/success",(req, res)=>{
+    console.log(req.body);
+    res.send("payment successfull");
 })
 
 
